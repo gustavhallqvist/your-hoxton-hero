9baa27e585f3cc896eb74da65a48a6cc36947e35
Kirsti McCabe <kirstmccabe@gmail.com>
2021-03-06 17:58:46 +0000

    bookings/index page as dashboard plus few other small fixes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,7 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = Booking.where(user: current_user).reverse
+    @bookings = policy_scope(Booking).order(created_at: :desc)
   end
 
   def new
