0d55d41a363cd7930248d8c75962be469748ba1f
Gentian Tare <gentiantare@gmail.com>
2021-03-09 18:15:54 +0000

    adding avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
   # end
 
   private
+   def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+    end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
